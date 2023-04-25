0e953a97673151c1bd4f6dfef2664077fd3f960e
theshaquib <114143938+theshaquib@users.noreply.github.com>
2023-04-25 16:24:56 +0530

    testing

---
+++ src/components/article/Article.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./article.css";
+
+const Article = ({ imgUrl, date, title }) => {
+  return (
+    <div className="gpt3__blog-container_article">
+      <div className="gpt3__blog-container_article-image">
+        <img src={imgUrl} alt="blog" />
+      </div>
+      <div className="gpt3__blog-container_article-content">
+      <div> <p>{date}</p>
+        <h3>{title}</h3>
+      </div>
+        <div>
+          <p>Reab Full Article</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
